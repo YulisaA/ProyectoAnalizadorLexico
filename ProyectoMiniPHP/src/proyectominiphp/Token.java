8c1ba35d2909b8adc5463a8f9025cbddf5f38dda
yulyazurdia8 <yulyazurdia8@hotmail.com>
2018-04-01 22:26:51 -0600

    Generate Lexer.java and added tokens for every regular expression used.

---
+++ ProyectoMiniPHP/src/proyectominiphp/Token.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectominiphp;
+
+/**
+ *
+ * @author DELL
+ */
+public enum Token {
+    RESERVEDWORDS, AROPERATOR, LOGOPERATOR, COMPOPERATOR, INCDECOPERATOR, ASSIGNOPERATOR,
+    BOOL, INT, DOUBLE, STRING, IDVAR, CONSTANT, PRECONSTANT, CONTROLSTRUCT, PREVAR, FUNCTION,
+    COMMENT, DB, PHP, NEWLINE, BLANKSPACE, PARENTHESIS, BRACKETS, KEYS, COMA, SEMICOLON,
+    CONCAT, ERROR
+}
---
+++ ProyectoMiniPHP/src/proyectominiphp/miniPHP.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectominiphp;
+
+import java.io.File;
+
+/**
+ *
+ * @author DELL
+ */
+public class miniPHP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String path = "C:/Users/DELL/Desktop/ProyectoAnalizadorLexico/ProyectoMiniPHP/src/proyectominiphp/Lexer.flex";
+        generarLexer(path);
+    }
+    public static void generarLexer(String path){
+        File file = new File(path);
+        jflex.Main.generate(file);
+}
+        
+    }
